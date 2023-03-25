840ad8fd1575f76d3ea6e9ee5840ce8e7bf9d4fb
TheVicttor <66380571+TheVicttor@users.noreply.github.com>
2023-03-25 15:47:37 -0300

    Add files via upload

---
+++ main/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.urls import reverse
+# Create your models here.
+class Categoria(models.Model):
+	nome = models.CharField(max_length=150, db_index=True)
+	slug = models.SlugField(max_length=150, unique=True, db_index=True)
+	data_criacao = models.DateTimeField(auto_now_add=True)
+	data_ultima_atualizacao = models.DateTimeField(auto_now=True)
+	
+	class Meta:
+		ordering = ('nome', )
+		verbose_name = 'categoria'
+		verbose_name_plural = 'categorias'
+	def __str__(self):
+		return self.nome
+	def get_absolute_url(self):
+		return reverse('main:listar_produtos_por_categoria', args=[self.slug])
+class Produto(models.Model):
+	categoria = models.ForeignKey(Categoria, related_name='produtos', on_delete=models.CASCADE)
+	nome = models.CharField(max_length=100, db_index=True)
+	slug = models.SlugField(max_length=100, db_index=True)
+	descricao = models.TextField(blank=True)
+	preco = models.DecimalField(max_digits=10, decimal_places=2)
+	disponivel = models.BooleanField(default=True)
+	estoque = models.PositiveIntegerField()
+	data_criacao = models.DateTimeField(auto_now_add=True)
+	data_ultima_atualizacao = models.DateTimeField(auto_now=True)
+	imagem = models.ImageField(upload_to='produtos/%Y/%m/%d', blank=True)
+	
+	class Meta:
+		ordering = ('nome', )
+		index_together = (('id', 'slug'),)
+	def __str__(self):
+		return self.nome
+	def get_absolute_url(self):
+		return reverse('main:detalhes_produto', args=[self.id, self.slug])
